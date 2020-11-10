8321b5a4a4b79bdd9f0500d7ea97e938f9e15419
Rajdeep Ghatore <r.ghatore@gmail.com>
2020-11-10 17:59:51 +0000

    refactored using ternary operator

--- main.rb
+++ main.rb
@@ -2,11 +2,11 @@ require './Player'
 require './Question'
 require './Turn'
 
-puts "Welcome to this Math game!"
-puts "Goal: sum the two random numbers provided."
-puts "Constraint: three incorrect answers and you lose the game!"
-puts "Try to speed up for fun!"
-puts "Ctrl + C to exit at any time."
+puts "Welcome to this Math game!
+Goal: sum the two random numbers provided.
+Constraint: three incorrect answers and you lose the game!
+Try to speed up for fun!
+Ctrl + C to exit at any time."
 
 print "Please enter name for player 1: "
 name1 = gets.chomp
@@ -19,15 +19,13 @@ question = Question.new
 turn = Turn.new
 
 while player1.lives > 0 && player2.lives > 0 do
-  puts "---#{player1.name}'s turn---" unless turn.even
-  puts "---#{player2.name}'s turn---" if turn.even
+  turn.even ? (puts "---#{player2.name}'s turn---") : (puts "---#{player1.name}'s turn---")
   question.prompt
   if question.validate
     puts "You're correct!"
   else
     puts "Oops, that is incorrect!"
-    player1.lives -= 1 unless turn.even
-    player2.lives -= 1 if turn.even
+    turn.even ? (player2.lives -= 1) : (player1.lives -= 1)
   end
   turn.update
   puts "#{player1.name}'s lives remaining: #{player1.lives}, #{player2.name}'s lives remaining: #{player2.lives}"
@@ -37,6 +35,3 @@ puts "#{player1.name} won with #{player1.lives} lives remaining!" if player1.liv
 puts "#{player2.name} won with #{player2.lives} lives remaining!" if player2.lives > 0
 puts "----- GAME OVER -----"
 puts "Thanks for playing!"
-
-
-
